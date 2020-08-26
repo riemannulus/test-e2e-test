24bbb18b0bb4a8342d9782a3ec15216ce3ed8352
Lee, Suho <riemannulus@hitagi.moe>
2020-08-26 01:16:49 -0700

    IniT

---
+++ main.py
@@ -0,0 +1,55 @@
+import re
+import time
+from os import popen
+
+import psutil
+from pywinauto import application, WindowSpecification
+
+
+def has_identifier(target: WindowSpecification, identifier: str):
+    ctrl_identifiers = target._ctrl_identifiers()
+    result = False
+    for ctrl in ctrl_identifiers.values():
+        if identifier in ctrl:
+            result = True
+    return result
+
+
+def start_process():
+    popen('NineChroniclesInstaller.exe')
+    time.sleep(1)
+    PID = 0
+    for proc in psutil.process_iter():
+        try:
+            pinfo = proc.as_dict(attrs=['pid', 'name'])
+        except psutil.NoSuchProcess:
+            pass
+        else:
+            if "NineChroniclesInstaller.tmp" == str(pinfo['name']):
+                PID = pinfo['pid']
+    return PID
+
+
+if __name__ == '__main__':
+    pid = start_process()
+    app = application.Application(backend='uia').connect(process=pid)
+    a = app.window(class_name='TWizardForm')
+    try:
+        a["Next >"].click()
+        if has_identifier(a, "Next >"):
+            a["Next >"].click()
+        a["Install"].click()
+
+        count = 0
+
+        while count < 10:
+            count += 1
+            if has_identifier(a, "Finish"):
+                a["Finish"].click()
+            else:
+                time.sleep(5)
+
+        if count >= 10:
+            raise Exception('Cannot installed in 50 second.')
+    except Exception as e:
+        print(e)
