ebbe86f6de52ffe297d217e298acd07038be9c1b
Lee, Suho <riemannulus@hitagi.moe>
2020-08-27 00:05:14 -0700

    게임 실행 여부까지 판별

--- main.py
+++ main.py
@@ -14,9 +14,13 @@ def has_identifier(target: WindowSpecification, identifier: str) -> bool:
     return result
 
 
-def start_process() -> int:
+def start_9c() -> int:
     popen('NineChroniclesInstaller.exe')
     time.sleep(1)
+    return get_pid("NineChroniclesInstaller.tmp")
+
+
+def get_pid(process_name: str) -> int:
     pid = 0
     for proc in psutil.process_iter():
         try:
@@ -24,13 +28,13 @@ def start_process() -> int:
         except psutil.NoSuchProcess:
             pass
         else:
-            if "NineChroniclesInstaller.tmp" == str(pinfo['name']):
+            if process_name == str(pinfo['name']):
                 pid = pinfo['pid']
     return pid
 
 
 if __name__ == '__main__':
-    pid = start_process()
+    pid = start_9c()
     app = application.Application(backend='uia').connect(process=pid)
     window: WindowSpecification = app.window(class_name='TWizardForm')
 
@@ -55,3 +59,10 @@ if __name__ == '__main__':
 
     if count >= 20:
         raise Exception('Cannot installed in 100 second.')
+
+    time.sleep(1)
+    if get_pid("Nine Chronicles.exe") == 0:
+        raise Exception('Cannot run NineChronicles Launcher.')
+    else:
+        print("Successfully Launched.")
+
